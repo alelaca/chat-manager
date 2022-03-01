a076e86d1a36bc72f68c0a39b3fcc6b3e1c61dab
Alejandro Lacassy <ale.lacassy@gmail.com>
2022-02-28 21:47:12 -0300

    adding tests

--- src/usecases/post/post_test.go
+++ src/usecases/post/post_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/alelaca/chat-manager/src/entities/dtos"
 	"github.com/alelaca/chat-manager/src/repository"
+	"github.com/alelaca/chat-manager/src/topics"
 )
 
 func TestCreatePost_OK(t *testing.T) {
@@ -18,6 +19,47 @@ func TestCreatePost_OK(t *testing.T) {
 
 	handler := Handler{
 		Repository: &repository.RepositoryMock{},
+	}
+
+	timestamp := time.Now()
+
+	post, err := handler.CreatePost(postDTO)
+
+	if err != nil {
+		t.Errorf("Test failed with error: '%s'", err.Error())
+	}
+
+	if post.ID == "" {
+		t.Errorf("Test failed. Post id not set")
+	}
+
+	if post.Message != postDTO.Message {
+		t.Errorf("Test failed. Post message expected '%s', got '%s'", postDTO.Message, post.Message)
+	}
+
+	if post.Sender != postDTO.Sender {
+		t.Errorf("Test failed. Post sender expected '%s', got '%s'", postDTO.Sender, post.Sender)
+	}
+
+	if post.Room != postDTO.Room {
+		t.Errorf("Test failed. Post room expected '%s', got '%s'", postDTO.Room, post.Room)
+	}
+
+	if post.CreatedDate.Before(timestamp) {
+		t.Errorf("Test failed. Post CreatedDate not configured properly")
+	}
+}
+
+func TestCreatePost_CommandOK(t *testing.T) {
+	postDTO := dtos.PostDTO{
+		Message: "/stock=aapl.us",
+		Sender:  "Jane",
+		Room:    "work-room",
+	}
+
+	handler := Handler{
+		TopicsHandler: &topics.TopicMock{},
+		Repository:    &repository.RepositoryMock{},
 	}
 
 	timestamp := time.Now()
@@ -78,6 +120,27 @@ func TestCreatePost_RepositoryFail(t *testing.T) {
 		Repository: &repository.RepositoryMock{
 			SavePostError: errors.New("repo error"),
 		},
+	}
+
+	_, err := handler.CreatePost(postDTO)
+
+	if err == nil {
+		t.Errorf("Test failed, error expected")
+	}
+}
+
+func TestCreatePost_CommandNotifyFail(t *testing.T) {
+	postDTO := dtos.PostDTO{
+		Message: "/stock=aapl.us",
+		Sender:  "Jane",
+		Room:    "work-room",
+	}
+
+	handler := Handler{
+		TopicsHandler: &topics.TopicMock{
+			NotifyMessageError: errors.New("error"),
+		},
+		Repository: &repository.RepositoryMock{},
 	}
 
 	_, err := handler.CreatePost(postDTO)

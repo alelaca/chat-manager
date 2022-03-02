289ac1af2bef526d467dd35d090cac7e204e42e7
Alejandro Lacassy <ale.lacassy@gmail.com>
2022-03-01 23:50:57 -0300

    configuration

--- src/main.go
+++ src/main.go
@@ -18,6 +18,7 @@ import (
 	rabbitmqtopics "github.com/alelaca/chat-manager/src/topics/rabbitmq"
 	"github.com/alelaca/chat-manager/src/usecases/post"
 	"github.com/streadway/amqp"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -53,6 +54,11 @@ func main() {
 	websocketHandler := websocket.InitializeWebsocketHandler(postHandler, authHandler)
 	worker := queues.InitializeWorker(queuesHandler, postHandler, websocketHandler)
 	router := routes.InitializeRouter(websocketHandler, authHandler)
+
+	if profile == ScopeIntgDocker || profile == ScopeLocal {
+		InitializeMongoData(mongodbClient)
+		InitializeRabbitMQInfra(rabbitmqClient, envConfig)
+	}
 
 	websocketHandler.StartPool()
 	worker.StartPollingPostsMessages()
@@ -96,4 +102,55 @@ func ConnectMongoDB(envConfig config.Config) *mongo.Client {
 	}
 
 	return client
+}
+
+// Methods only for local and docker compose!! because docker files couldn't be loaded directly from docker
+func InitializeMongoData(mongo *mongo.Client) {
+	mongo.Database("chatdb").Collection("users").InsertMany(context.Background(), []interface{}{
+		bson.M{"_id": "1221a07d-6ee9-4d60-92e7-46850e7565a2", "username": "Bob", "password": "bcb15f821479b4d5772bd0ca866c00ad5f926e3580720659cc80d39c9d09802a"},
+		bson.M{"_id": "3f68696f-cf68-4d8e-a440-05230f3e1a4f", "username": "Alice", "password": "4cc8f4d609b717356701c57a03e737e5ac8fe885da8c7163d3de47e01849c635"},
+		bson.M{"_id": "1cd8e021-1921-4d85-abe9-323acf326656", "username": "Jane", "password": "68487dc295052aa79c530e283ce698b8c6bb1b42ff0944252e1910dbecdc5425"},
+		bson.M{"_id": "f3690232-27c0-425c-9962-8828d32ee4eb", "username": "Justin", "password": "69f7f7a7f8bca9970fa6f9c0b8dad06901d3ef23fd599d3213aa5eee5621c3e3"},
+	})
+}
+
+// Methods only for local and docker compose!! because docker files couldn't be loaded directly from docker
+func InitializeRabbitMQInfra(rabbit *amqp.Connection, envConfig config.Config) {
+	ch, _ := rabbit.Channel()
+	defer ch.Close()
+
+	ch.ExchangeDeclare(
+		envConfig.RabbitMQ.MessagesTopic.Name, // name
+		"topic",                               // type
+		true,                                  // durable
+		false,                                 // auto-deleted
+		false,                                 // internal
+		false,                                 // no-wait
+		nil,                                   // arguments
+	)
+
+	ch.QueueDeclare(
+		envConfig.RabbitMQ.PostsQueue.Name, // name
+		true,                               // durable
+		false,                              // delete when unused
+		false,                              // exclusive
+		false,                              // no-wait
+		nil,                                // arguments
+	)
+
+	ch.QueueDeclare(
+		envConfig.RabbitMQ.CommandQueue.Name, // name
+		true,                                 // durable
+		false,                                // delete when unused
+		false,                                // exclusive
+		false,                                // no-wait
+		nil,                                  // arguments
+	)
+
+	ch.QueueBind(
+		envConfig.RabbitMQ.CommandQueue.Name,  // queue name
+		"stock.#",                             // routing key
+		envConfig.RabbitMQ.MessagesTopic.Name, // exchange
+		false,
+		nil)
 }
